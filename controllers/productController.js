c86fc20577366b5c0782d9350c5f07cb9794efa8
Isaac Dalyop <50559275+Dalyop@users.noreply.github.com>
2025-12-23 14:56:20 +0100

    Remove prisma import

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,5 @@
 import prisma from '../prismaClient.js';
 import pluralize from 'pluralize';
-import prisma from '../prismaClient.js';
 
 // Get all products with advanced filtering and search
 export const getProducts = async (req, res) => {
